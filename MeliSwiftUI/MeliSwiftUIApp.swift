753f33c44be9e1b68a3ac06bbe4a2889f612bbc8
PVillamilJusto <pedro.villamil@justo.mx>
2024-08-31 11:11:34 -0500

    Initial Commit

---
+++ MeliSwiftUI/MeliSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliSwiftUIApp.swift
+//  MeliSwiftUI
+//
+//  Created by Pedro Andres Villamil on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MeliSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
